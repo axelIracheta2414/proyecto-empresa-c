0012c501a5e321fcec9c31347f4ad49dac18cf9b
Axel GaNu <naxel2414@gmail.com>
2026-04-08 16:11:22 -0300

    agregado de texto

--- archiveitor.py
+++ archiveitor.py
@@ -1,2 +1,4 @@
 print("aca es donde empieza el archivo con su titulo")
 print("este print viene desde git hub")
+
+print("y este es otro texto apra practicar la sincononicidad entre visual code y github")
